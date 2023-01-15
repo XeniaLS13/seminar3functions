9e5fb527291adb41e26faeab3cd02cc468d1c22b
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-15 23:19:40 +0300

    Поиск третьей цифры заданного числа

---
+++ analysis4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+int number = 123456895;
+    if(number<100)
+{
+    Console.WriteLine("неподходящее число");
+}
+else
+{
+while (number > 100)
+{
+    number = number /10;
+    Console.WriteLine(number);
+}
+Console.WriteLine(number % 10);
+}
