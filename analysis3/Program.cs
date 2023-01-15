c3163ae9c176139dfd80ec21d7f099a78ecb56b9
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-15 23:04:31 +0300

    Решение задания по поиску второй цифры пятизначного числа

---
+++ analysis3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Если цифр больше 3 (5)
+//           КТСДЕ  
+//int number = 12345;
+//Console.WriteLine(number % 100 / 10);
+
+// Решение задания по поиску второй цифры пятизначного числа
+
+int number = 12345;
+
+while(number > 100)
+{
+    number = number / 10;
+    Console.WriteLine(number);
+}
+Console.WriteLine(number % 10);
