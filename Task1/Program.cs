db41bd216a128c0b26df091c8b23ae15c63ce346
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-15 23:51:15 +0300

    Программа по поиску расстояния между 2 точек

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты
+//двух точек и находит расстояние между ними в 2D пространстве. 
+// * A(3,6), B(2,1) ->5,09
+// * A(7,-5), B(1, -1)-> 7,21
+
+int a1 = 3;
+int a2 = 2;
+int b1 = 6;
+int b2 = 1;
+
+double distance = GetDistance(a1, a2, b1, b2);
+Console.WriteLine(distance);
+
+double GetDistance(int a1, int a2, int b1, int b2)
+{
+    return Math.Sqrt((a1 - a2) * (a1 - a2) + (b1 - b2) * (b1 - b2));
+}
